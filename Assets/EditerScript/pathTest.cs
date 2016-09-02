1308ef4368c485e32f3ae1ec2177ea353b10179e
AlayaElla <515451867@qq.com>
2016-09-02 22:49:26 +0800

    增路径编辑的功能，在地图中测试了一下。 TODO：分支路点，采集地识别功能

--- Assets/EditerScript/pathTest.cs
+++ Assets/EditerScript/pathTest.cs
@@ -7,11 +7,13 @@ public class pathTest : MonoBehaviour
 
     public Transform pathpoint;
 	public Transform root;
+    public Transform Listparent;
     Transform[] trans;
     LTSpline cr;
     public int Accuracy = 100;
 
     public bool showspline = true;
+    public bool isDraw = true;
 
 	void OnEnable(){
 		// create the path
@@ -37,32 +39,39 @@ public class pathTest : MonoBehaviour
         if (cr != null && showspline)
         {
             cr.gizmoDraw(); // To Visualize the path, use this method
-            DrawLine();
         }
+        if (isDraw)
+            DrawLine();
     }
 
     void DrawLine()
     {
-        if (transform.Find("/pathLine") != null)
-        {
-            Tools.DestroyImmediate(transform.Find("/pathLine").gameObject);
-        }
+        CleanLine();
 
         GameObject pathLine = new GameObject();
         pathLine.name = "pathLine";
+        pathLine.transform.SetParent(Listparent);
 
         for (int i = 0; i < cr.ptsAdjLength; i += Accuracy)
         {
             Transform t = Tools.Instantiate(pathpoint);
             t.position = cr.ptsAdj[i];
             t.SetParent(pathLine.transform);
+
+            if (i + Accuracy < cr.ptsAdjLength)
+            {
+                Vector3 v3Dir = cr.ptsAdj[i + Accuracy] - t.position;
+                float angle = Mathf.Atan2(v3Dir.y, v3Dir.x) * Mathf.Rad2Deg;
+                t.eulerAngles = new Vector3(0, 0, angle);
+            }
         }
     }
 
     [ContextMenu("CleanLine")]
     void CleanLine()
     {
-        Tools.DestroyImmediate(transform.Find("/pathLine").gameObject);
+        if (transform.Find("/Canvas/Scroll View/Viewport/Content/pathLine") != null)
+            Tools.DestroyImmediate(transform.Find("/Canvas/Scroll View/Viewport/Content/pathLine").gameObject);
     }
 
 }
